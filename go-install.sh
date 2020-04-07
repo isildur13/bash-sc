c2d58fcce2b54572d3b357474090c9a37414a0c1
panchalyash13 <panchalyash13@gmail.com>
2020-04-07 23:05:41 +0530

    cleared mistakes

--- go-install.sh
+++ go-install.sh
@@ -7,7 +7,7 @@
 VERSION=$1
 OS=$2
 ARCH=$3
-wget -O go$VERSION.$OS-$ARCH https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+wget -O go$VERSION.$OS-$ARCH.tar.gz https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
 tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
 rm go$VERSION.$OS-$ARCH.tar.gz
