087d1df6c50d05a0752887e32319224b77a61a89
isildur13 <panchalyash13@gmail.com>
2020-05-02 10:01:40 +0530

    node installer: Takes default value incase user doesnot enter ARGS like node version, OS, Arch.

--- node-install.sh
+++ node-install.sh
@@ -1,11 +1,16 @@
-#/bin/bash 
+#/bin/bash
 
-NODE_VERSION="12.16.3"
-OS="linux"
-ARCH="x64"
+NODE_VERSION=${1:-"12.16.3"}
+OS=${2:-"linux"}
+ARCH=${3:-"x64"}
 
-wget https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-$OS-$ARCH.tar.xz       tar xf node-v$NODE_VERSION-$OS-$ARCH.tar.xz
-rm node-v$NODE_VERSION-$OS-$ARCH.tar.xz
+wget https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-$OS-$ARCH.tar.xz
+tar xf node-v$NODE_VERSION-$OS-$ARCH.tar.xz
 sudo cp -a node-v$NODE_VERSION-$OS-$ARCH /opt/node
-rm node-v$NODE_VERSION-$OS-$ARCHi
-cd /bin                                                                                ln -s /opt/node/bin/node node                                                          ln -s /opt/node/bin/npm npm                                                            ln -s /opt/node/bin/npx npx  
+
+rm -rf node-v$NODE_VERSION-$OS-$ARCH
+rm node-v$NODE_VERSION-$OS-$ARCH.tar.xz
+cd /bin
+ln -s /opt/node/bin/node node
+ln -s /opt/node/bin/npm npm
+ln -s /opt/node/bin/npx npx
